374122c434f0ac0bf287bdf4723101199a878843
Vasyl Kaminskyi <vasia1415@gmail.com>
2022-07-10 11:00:40 +0300

    Add project files.

---
+++ ариф операції.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Учоба
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double FirstValue, SecondValue, ThirdValue;
+            System.Console.WriteLine("Type your first value");
+            FirstValue = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Type your second value");
+            SecondValue = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Type your third value");
+            ThirdValue = double.Parse(Console.ReadLine());
+            double sumresult = (FirstValue + SecondValue + ThirdValue);
+            double multresult = (FirstValue * SecondValue * ThirdValue);
+            System.Console.WriteLine("Your summ result is: " + sumresult);
+            System.Console.WriteLine("Your mult result is: " + multresult);
+            Console.ReadLine();
+        }
+    }
+}
