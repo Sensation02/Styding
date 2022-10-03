bf3c78c89974b55a712ad45adcf0c24f80ed2041
Sensation02 <106180642+Sensation02@users.noreply.github.com>
2022-10-03 21:13:30 +0300

    Add files via upload
    unfinished

---
+++ 23.Boxing unboxing/benchmark.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+namespace _23.Boxing_unboxing
+{
+    public class BoxingTest
+    {
+        [Benchmark]
+        public void NoBoxing() //швидше
+        {
+            int res = 0;
+            int a = 1;
+            res += a;
+        }
+
+        [Benchmark]
+        public void Boxing() //довше
+        {
+            int res = 0;
+            object a = 1;
+            res += (int)a;
+        }
+    }
+}
---
+++ 23.Boxing unboxing/boxing unboxing.cs
@@ -0,0 +1,23 @@
+﻿namespace Уроки
+{
+    /*
+     *  Упаковка і розпаковка значимих типів (boxin unboxing)
+     *      це спливає на швидкодію комп"ютера, при чому в гіршу сторону
+     *
+     *
+     *
+     *
+     */
+
+    internal class leson
+    {
+        private static void Main(string[] args)
+        {
+            int a = 1;
+            object b = a; //так ми упаковуємо перемінну типа інт в тип об"єкт
+            int c = (int)b; //розпаковка
+
+            decimal d = (decimal)b; //при спробі розпаковки з інт в децимал отримуємо InvalidCastException
+        }
+    }
+}
