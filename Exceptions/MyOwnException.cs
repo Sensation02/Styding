2e1a9db9c65a4617d1bb2404a30cc7f8ba5959d1
Sensation02 <106180642+Sensation02@users.noreply.github.com>
2022-10-03 21:24:14 +0300

    Add files via upload

---
+++ Exceptions/MyOwnException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class MyOwnException : Exception
+        //можна успадковуватися від будь якого іншого ексепшена
+    {
+        public MyOwnException() : base("це моя помилка")
+        {
+            
+        }
+
+        public MyOwnException(string? message) : base(message)
+        {
+        }
+
+        public MyOwnException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
