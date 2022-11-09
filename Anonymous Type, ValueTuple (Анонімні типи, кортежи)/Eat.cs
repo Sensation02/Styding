5c583cb03386f626631bd4eecbcd47bec39c9d0a
Sensation02 <106180642+Sensation02@users.noreply.github.com>
2022-11-09 11:16:56 +0200

    Add files via upload

---
+++ Anonymous Type, ValueTuple (Анонімні типи, кортежи)/Eat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Type__ValueTuple__Анонімні_типи__кортежи_
+{
+    internal class Eat
+    {
+        public string Name { get; set; }
+    }
+}
